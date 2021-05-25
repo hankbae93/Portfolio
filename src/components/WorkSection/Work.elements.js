b67cbb5e009e434c7a5782876bea41d068d71701
Ranja Func <seolin9340@gmail.com>
2021-05-25 19:37:09 +0900

    Project 컴포넌트 구현 완성

--- src/components/WorkSection/Work.elements.js
+++ src/components/WorkSection/Work.elements.js
@@ -3,10 +3,16 @@ import { Heading, Section } from '../../globalStyle';
 import { AboutInfoTech } from '../AboutSection/About.elements';
 
 export const WorkWrapper = styled(Section)`
+    height: auto;
 
+    @media screen and (max-width: 1300px) {
+        padding: 10vh 0 0;
+        box-sizing: content-box;
+    }
 `;
 
 export const WorkTitle = styled(Heading)`
+    margin: 30px 0;
     text-align: center;
 `;
 
@@ -22,9 +28,15 @@ export const Project = styled.div`
     justify-content: space-between;
     align-items: center;
     padding: 25px 10px;    
+    margin: 0 0 200px;
+
+    @media screen and (max-width: 1300px) {
+        justify-content: center;
+        flex-direction: column;
+    }
 `;
 
-export const ProjectTitle = styled.h3`    
+export const ProjectTitle = styled.h3`        
     color: #ffab12;    
     font-size: 2em;    
 
@@ -35,14 +47,24 @@ export const ProjectTitle = styled.h3`
 
 export const ProjectImg = styled.img`
     overflow:hidden;
+    display: inline-block;
     width: 50%;
     border-radius: 15px;
     box-shadow: 1px 1px 5px #000;
+
+    @media screen and (max-width: 1300px) {
+        margin: 0 0 20px;
+        min-width: 600px;
+    }
+    
 `;
 
 export const ProjectDesc = styled.div`
     width: 50%;
     padding:  10px 25px;
+    @media screen and (max-width: 1300px) {        
+        min-width: 600px;
+    }
 `;
 
 export const ProjectTech = styled.p`
@@ -50,4 +72,24 @@ export const ProjectTech = styled.p`
 `;
 
 export const ProjectTechInfo = styled(AboutInfoTech)`
+`;
+
+export const ProjectDescription = styled.p`    
+`;
+
+export const ProjectSubTitle = styled.span`
+    display: block;
+    color: #ccc;
+    margin-right: 15px;
+`;
+
+export const ProjectLinks = styled.div`
+    padding: 10px 0;
+`;
+
+export const ProjectLink = styled.a`
+    display: inline-block;
+    margin-right: 15px;
+    font-size: 2.25em;
+    color: #fff;
 `;
