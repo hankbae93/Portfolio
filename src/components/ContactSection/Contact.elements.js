c4878e29e8114bd7344488daccb11af452fd64dd
Ranja Func <seolin9340@gmail.com>
2021-05-27 12:16:54 +0900

    Contact 섹션 스타일링 완성

--- src/components/ContactSection/Contact.elements.js
+++ src/components/ContactSection/Contact.elements.js
@@ -2,14 +2,24 @@ import styled from 'styled-components';
 import { Heading, Section } from '../../globalStyle';
 
 export const ContactWrapper = styled.footer`
-    background-color: #fff;
-    color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50vh;        
 `;
 
 export const ContactHeading = styled(Heading)`
 
 `;
 
-export const ContactUl = styled.ul``;
+export const ContactUl = styled.ul`    
+    padding: 0 0 0 15px;
+`;
 export const ContactLi = styled.li`
+    padding: 10px;
+    margin-bottom: 10px;
+    color: #000;
+    background-color: #fff;
+    border-radius: 15px;
 `;
--- src/components/ContactSection/Contact.js
+++ src/components/ContactSection/Contact.js
@@ -12,13 +12,13 @@ const Contact = () => {
             <ContactHeading>Contact</ContactHeading>
             <ContactUl>
                 <ContactLi>
-                    깃허브 : 
+                    깃허브 : <a href="https://github.com/ranjafunc">https://github.com/ranjafunc</a>
                 </ContactLi>
                 <ContactLi>
-                    전화번호 : 
+                    전화번호 : 010-4399-9340
                 </ContactLi>
                 <ContactLi>
-                    이메일 : 
+                    이메일 : cjstjekd23@naver.com
                 </ContactLi>
             </ContactUl>
         </ContactWrapper>   
